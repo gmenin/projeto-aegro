b8e4404ea1aed74b717c8fbbfc78d762a9c3a408
Gabriel Menin <gabriel.menin@aegro.com.br>
2022-04-18 08:39:38 -0300

    Create project

---
+++ src/main/java/com/aegro/projetoaegro/ProjetoAegroApplication.java
@@ -0,0 +1,13 @@
+package com.aegro.projetoaegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAegroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aegro/projetoaegro/ProjetoAegroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegro.projetoaegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAegroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
