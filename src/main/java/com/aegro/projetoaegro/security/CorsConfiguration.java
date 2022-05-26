593c4258bca8033f0fb4ef61ea94fdbeb6a976d7
Gabriel Menin <gabriel.menin@aegro.com.br>
2022-05-26 07:17:23 -0300

    fix: create cors config file

---
+++ src/main/java/com/aegro/projetoaegro/security/CorsConfiguration.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.aegro.projetoaegro.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Gabriel Menin
+ *
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
